5bd9788977728876ec7f752eae89a5e213555eb6
sbagos27 <sbagos@csumb.edu>
2025-03-18 01:55:33 -0700

    commit 3:24:46
    scriptable object huh

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,8 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
+    
     public void Interact() {
         Debug.Log("Interact");
+        Transform kitchenObjectTransfrom =  Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTransfrom.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObjectTransfrom.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
     }
 }
---
+++ Assets/Scripts/KitchenObject.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class KitchenObject : MonoBehaviour {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    public KitchenObjectSO GetKitchenObjectSO() {
+        return kitchenObjectSO;
+    }
+}
